e1416074ef1f2301a65363d46b2335f8038169ba
Adrian Fernandes <adrianancelmo1098@gmail.com>
2022-05-04 21:57:52 -0300

    Aula 22 - Sobrescrevendo handler do Spring

--- src/main/java/academy/devdojo/springboot2/requests/LivroPostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/LivroPostRequestBody.java
@@ -3,7 +3,6 @@ package academy.devdojo.springboot2.requests;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class LivroPostRequestBody {
