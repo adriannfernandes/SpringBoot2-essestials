f5bffffea44cf47fbbed172b0d3f87fe42d46bc3
Adrian Fernandes <adrianancelmo1098@gmail.com>
2022-05-07 17:24:39 -0300

    Aula 31 - Spring Data JPA Test / 02

--- src/test/java/academy/devdojo/springboot2/repository/LivroRepositoryTest.java
+++ src/test/java/academy/devdojo/springboot2/repository/LivroRepositoryTest.java
@@ -1,12 +1,16 @@
 package academy.devdojo.springboot2.repository;
 
 import academy.devdojo.springboot2.domain.Livro;
+import ch.qos.logback.core.net.AbstractSSLSocketAppender;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,15 +22,83 @@ class LivroRepositoryTest {
     private LivroRepository livroRepository;
 
     @Test
-    @DisplayName("Save creates livro when sucessful")
+    @DisplayName("Save persists livro when sucessful")
     void save_PersistLivro_WhenSucessful(){
 
         Livro livroToBeSaved = createLivro();
+
         Livro livroSaved = this.livroRepository.save(livroToBeSaved);
 
         Assertions.assertThat(livroSaved).isNotNull();
+
         Assertions.assertThat(livroSaved.getId()).isNotNull();
+
         Assertions.assertThat(livroSaved.getName()).isEqualTo(livroToBeSaved.getName());
+
+    }
+
+
+    @Test
+    @DisplayName("Save updates livro when sucessful")
+    void save_UpdatesLivro_WhenSucessful(){
+
+        Livro livroToBeSaved = createLivro();
+
+        Livro livroSaved = this.livroRepository.save(livroToBeSaved);
+
+        livroSaved.setName("O Poder do hábito");
+
+        Livro livroUpdated = this.livroRepository.save(livroSaved);
+
+        Assertions.assertThat(livroUpdated).isNotNull();
+
+        Assertions.assertThat(livroUpdated.getId()).isNotNull();
+
+        Assertions.assertThat(livroUpdated.getName()).isEqualTo(livroSaved.getName());
+
+    }
+
+    @Test
+    @DisplayName("Delete removes livro when sucessful")
+    void delete_RemovesLivro_WhenSucessful(){
+
+        Livro livroToBeSaved = createLivro();
+
+        Livro livroSaved = this.livroRepository.save(livroToBeSaved);
+
+        this.livroRepository.delete(livroSaved);
+
+        Optional<Livro> livroOptional = this.livroRepository.findById(livroSaved.getId());
+
+        Assertions.assertThat(livroOptional).isEmpty();
+
+    }
+
+    @Test
+    @DisplayName("Find By Name returns list of livro when sucessful")
+    void findByName_ReturnsListOfLivro_WhenSucessful(){
+
+        Livro livroToBeSaved = createLivro();
+
+        Livro livroSaved = this.livroRepository.save(livroToBeSaved);
+
+        String name = livroSaved.getName();
+
+        List<Livro> foundLivros = this.livroRepository.findByName(name);
+
+        Assertions.assertThat(foundLivros).isNotEmpty();
+
+        Assertions.assertThat(foundLivros).contains(livroSaved);
+
+    }
+
+    @Test
+    @DisplayName("Find By Name returns empty list when no livro is found")
+    void findByName_ReturnsEmptyList_WhenLivroIsNotFound(){
+
+        List<Livro> foundLivros = this.livroRepository.findByName("xxxxxx");
+
+        Assertions.assertThat(foundLivros).isEmpty();
 
     }
 
